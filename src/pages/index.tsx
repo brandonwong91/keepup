7a68901b688675c8142942e4524d5639b86b60b1
Brandon Wong <brandonwongck@gmail.com>
2023-03-25 16:11:46 +0800

    add auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
+import { SignIn, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+// import Link from "next/link";
 
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
+  console.log("user", user);
   return (
     <>
       <Head>
@@ -15,38 +17,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+        {!user.isSignedIn && <SignIn />}
+        {!!user.isSignedIn && user.user.firstName && (
+          <div className="flex h-20 flex-col gap-x-1 rounded-lg bg-gray-100 p-2 text-slate-400">
+            {`Hi there, ${user.user.firstName}`}
+            <div className="self-center rounded-lg border-2 p-2 hover:border-2 hover:border-red-300 hover:text-red-300">
+              <SignOutButton />
+            </div>
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+        )}
       </main>
     </>
   );
