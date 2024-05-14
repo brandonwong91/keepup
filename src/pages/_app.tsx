9e1902f5a68be93f6c44bceaf88b52afabf34443
Brandon Wong <brandonwongck@gmail.com>
2024-05-14 18:14:32 +0800

    Moved previous work to geist page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,12 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { GeistProvider, CssBaseline } from "@geist-ui/core";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <GeistProvider>
-      <CssBaseline />
-      <ClerkProvider {...pageProps}>
-        <Component {...pageProps} />
-      </ClerkProvider>
-    </GeistProvider>
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
   );
 };
 
