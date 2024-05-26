5372d739eee51cfb878265d39ac935f602ec040b
Brandon Wong <brandonwongck@gmail.com>
2024-05-26 19:03:39 +0800

    Add check for auth on main page

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -3,8 +3,15 @@ import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 
 import React, { useRef } from "react";
 import { Button } from "./ui/button";
+import { SignedOut, SignInButton, SignedIn, useUser } from "@clerk/nextjs";
+import {
+  GlowingStarsBackgroundCard,
+  GlowingStarsDescription,
+  GlowingStarsTitle,
+} from "./ui/glowing-stars";
 
 const Confetti = () => {
+  const user = useUser();
   const controller = useRef<{ shoot: () => void } | null>(null);
   const onInitHandler = ({ conductor }: { conductor: any }) => {
     controller.current = conductor;
@@ -16,8 +23,21 @@ const Confetti = () => {
   };
   return (
     <div>
-      <Button onClick={onShoot}>Party time</Button>
-      <Fireworks onInit={onInitHandler} />
+      <SignedOut>
+        <GlowingStarsBackgroundCard>
+          <GlowingStarsTitle className="container flex justify-center space-x-2 pt-6">
+            <p>Let's get you</p>
+            <Button variant={"secondary"}>
+              <SignInButton />
+            </Button>
+          </GlowingStarsTitle>
+          <GlowingStarsDescription></GlowingStarsDescription>
+        </GlowingStarsBackgroundCard>
+      </SignedOut>
+      <SignedIn>
+        <Button onClick={onShoot}>Time to party {user.user?.firstName}</Button>
+        <Fireworks onInit={onInitHandler} />
+      </SignedIn>
     </div>
   );
 };
